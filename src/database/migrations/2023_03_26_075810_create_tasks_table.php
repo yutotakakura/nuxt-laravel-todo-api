5c8059c3d7ea49f77dc9ef9d1796470b1ca544e5
Yuto Takakura <yutotakakura0320@gmail.com>
2023-03-26 17:39:41 +0900

    APIを実装

---
+++ src/database/migrations/2023_03_26_075810_create_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->string('content', 100);
+            $table->string('person_in_charge', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
